036e432c547283de6a727ec875d4dead41db4359
Camilo-Silva <128434548+Camilo-Silva@users.noreply.github.com>
2023-05-19 19:03:02 -0300

    First commit Fragment

---
+++ app/src/main/java/com/example/ejerciciofragment/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.ejerciciofragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var floatingButton: FloatingActionButton
+    private var isModoLight: Boolean = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        floatingButton = findViewById(R.id.floatingActionButton)
+
+        floatingButton.setOnClickListener {
+            if (isModoLight){
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, DarkFragment()).commit()
+
+            }else{
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, BlankFragment()).commit()
+            }
+            isModoLight = !isModoLight
+        }
+    }
+}
